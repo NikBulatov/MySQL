23dddf11a50dc4f686c340a3b574825dcb8b5013
Nikita <anthrowolf@ya.ru>
2021-11-11 12:35:14 +0300

    11.11

--- Scripts/Script.sql
+++ Scripts/Script.sql
@@ -1,5 +1,5 @@
 DROP DATABASE IF EXISTS site;
-CREATE DATABASE IF NO EXISTS site;
+CREATE DATABASE IF NOT EXISTS site;
 USE site;
 
 -- create dictonary!!
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS messages
 	to_user_id BIGINT UNSIGNED NOT NULL,
 	created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
 	body TEXT NOT NULL,
-	PRIMARY KEY (from_user_id, to_user_id)
+	PRIMARY KEY (from_user_id, to_user_id),
 	FOREIGN KEY (from_user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
 	FOREIGN KEY (to_user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
@@ -40,5 +40,5 @@ CREATE TABLE IF NOT EXISTS requestes
 	id SERIAL PRIMARY KEY,
 	type_request ENUM ('Service', 'Question'),
 	message_id BIGINT UNSIGNED NOT NULL COMMENT 'Сообщение пользоватля',
-	FOREIGN KEY (message_id) REFERENCES messages(id) ON UPDATE CASCADE ON DELETE CASCADE
+	FOREIGN KEY (message_id) REFERENCES messages() ON UPDATE CASCADE ON DELETE CASCADE
 );
