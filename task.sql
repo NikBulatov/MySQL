a283510f5b615be9d29db5bc361ce589b8bfe73f
Nikita <anthrowolf@ya.ru>
2021-11-20 13:22:28 +0300

    Домашнее задание

--- task.sql
+++ task.sql
@@ -1,33 +1,3 @@
-USE vk;
 /*
- Домашнее задание к 4 уроку "CRUD-операции"
- 1. Заполнить все таблицы БД vk данными (по 10-100 записей в каждой таблице). файл filldb-deta.sql
- */
-INSERT INTO wallet(owner_id)
-SELECT id FROM users;
-/*
-2. Написать скрипт, возвращающий список имен (только firstname) пользователей без повторений в алфавитном порядке.
+https://docs.google.com/document/d/1yE0wn1m-EwqcJ458ga3eCptvBJcSIi1HaBp48OlT5ZI/edit
 */
-SELECT DISTINCT firstname
-FROM users
-ORDER BY firstname;
-
--- 3. Первые пять пользователей пометить как удаленные.
-UPDATE users
-SET is_deleted = 1
-ORDER BY firstname
-LIMIT 5;
-SELECT firstname, is_deleted
-FROM users
-ORDER BY firstname
-LIMIT 5;
--- 4. Написать скрипт, удаляющий сообщения «из будущего» (дата больше сегодняшней).
-DELETE
-FROM messages
-WHERE created_at > NOW();
-SELECT *
-FROM messages
-WHERE created_at < NOW()
-ORDER BY created_at;
--- 5. Написать название темы курсового проекта
--- Сайт мастера по услугам красоты
