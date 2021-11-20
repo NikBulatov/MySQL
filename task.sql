0aff87eacd4f00dd9b391c5b597938e41956c58c
Nikita Bulatov <87034809+NikBulatov@users.noreply.github.com>
2021-11-20 10:16:16 +0300

    Работа над ошибками
    Варианты решения 2, 3, 4 задачи.

--- task.sql
+++ task.sql
@@ -7,19 +7,30 @@ INSERT INTO wallet(owner_id)
 SELECT id FROM users;
 /*
 2. Написать скрипт, возвращающий список имен (только firstname) пользователей без повторений в алфавитном порядке.
+Первый вариант
 */
 SELECT DISTINCT firstname
 FROM users
 ORDER BY firstname;
+-- Второй вариант
+SELECT firstname
+FROM users
+GROUP BY firstname
+ORDER BY firstname;
 
 -- 3. Первые пять пользователей пометить как удаленные.
+-- Первый вариант
 UPDATE users
-SET is_deleted = 1
+SET is_deleted = b'1'
 ORDER BY firstname
 LIMIT 5;
 SELECT firstname, is_deleted
 FROM users
 ORDER BY firstname
+LIMIT 5;
+-- Второй вариант
+UPDATE users
+SET is_deleted = b'1'
 LIMIT 5;
 -- 4. Написать скрипт, удаляющий сообщения «из будущего» (дата больше сегодняшней).
 DELETE
