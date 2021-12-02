cada112e69b0d6926d006a19b2fbeba2659bd5f5
Nikita <anthrowolf@ya.ru>
2021-12-02 10:23:04 +0300

    Работа над ошибками

--- Scripts/task.sql
+++ Scripts/task.sql
@@ -12,8 +12,14 @@ SET created_at = '2005-12-06 13:56:02',
 
 -- 1. В таблице users поля created_at и updated_at оказались незаполненными. Заполните их текущими датой и временем
 UPDATE profiles
+SET created_at = NULL,
+    updated_at = NULL;
+UPDATE profiles
 SET created_at = NOW(),
     updated_at = NOW();
+
+SELECT user_id, created_at, updated_at
+FROM profiles;
 /*
 2. Таблица users была неудачно спроектирована.
 Записи created_at и updated_at были заданы типом VARCHAR и в них долгое время помещались значения в формате 20.10.2017 8:10.
@@ -31,7 +37,14 @@ SET created_at = '20.10.2017 8:10',
 UPDATE profiles
 SET created_at = STR_TO_DATE(created_at, '%d.%m.%Y %k:%i'),
     updated_at = STR_TO_DATE(updated_at, '%d.%m.%Y %k:%i');
-
+DESCRIBE profiles;
+ALTER TABLE profiles
+    CHANGE created_at created_at DATETIME DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE profiles
+    CHANGE updated_at updated_at DATETIME DEFAULT CURRENT_TIMESTAMP;
+DESCRIBE profiles;
+SELECT user_id, created_at, updated_at
+FROM profiles;
 /*
  3. В таблице складских запасов storehouses_products в поле value могут встречаться самые разные цифры:
  0, если товар закончился и выше нуля, если на складе имеются запасы.
@@ -42,6 +55,7 @@ USE storehouse;
 SELECT value
 FROM storehouses_products
 ORDER BY IF(value = 0, 1, 0), value;
+-- ORDER BY IF(value > 0, 0, 1), value;
 
 /*
  4. (по желанию) Из таблицы users необходимо извлечь пользователей, родившихся в августе и мае.
@@ -56,6 +70,7 @@ FROM profiles
 WHERE MONTH(birthday) = 05
    OR MONTH(birthday) = 08
 ORDER BY MONTH(birthday);
+-- разбор: SELECT name FROM users WHERE DATE_FORMAT(birthday_at, '%M') IN ('may', 'august');
 /*
  5. (по желанию) Из таблицы catalogs извлекаются записи при помощи запроса.
  SELECT * FROM catalogs WHERE id IN (5, 1, 2);
@@ -68,21 +83,18 @@ CREATE TABLE catalogs
     id      SERIAL PRIMARY KEY,
     catalog VARCHAR(255)
 );
-SELECT value
+-- разбор задания
+SELECT id, catalog
 FROM catalogs
 WHERE id IN (5, 1, 2)
-ORDER BY id;
-SELECT value
-FROM catalogs
-WHERE id = 1
-   OR id = 2
-   OR id = 5
-ORDER BY id;
+order by FIELD(id, 5, 1, 2);
 -- Практическое задание теме «Агрегация данных»
 /*
 1. Подсчитайте средний возраст пользователей в таблице users.
  */
-SELECT ROUND(AVG((TO_DAYS(NOW()) - TO_DAYS(birthday)) / 365.25), 3) AS 'Mean age'
+-- совпало с разбором
+USE vk;
+SELECT ROUND(AVG(TIMESTAMPDIFF(YEAR, birthday, NOW()))) AS 'Mean age'
 FROM profiles;
 /*
  2. Подсчитайте количество дней рождения, которые приходятся на каждый из дней недели.
@@ -92,16 +104,24 @@ FROM profiles;
  2.3 Считаем количество по дням недели и группируем по дням
  */
 USE vk;
-SELECT day_week, COUNT(new_date)
+SELECT day_week, COUNT(new_date) AS total
 FROM (SELECT new_date, DATE_FORMAT(new_date, '%W') AS day_week
       FROM (SELECT birthday + INTERVAL (YEAR(NOW()) - YEAR(birthday)) YEAR AS new_date
             FROM profiles) birthday_now) days_week
-GROUP BY day_week;
+GROUP BY day_week
+ORDER BY total DESC ;
+-- разбор задания
+SELECT COUNT(*)                                                                             AS total,
+       DATE_FORMAT(DATE(CONCAT_WS('-', YEAR(NOW()), MONTH(birthday), DAY(birthday))), '%W') AS day
+from profiles
+GROUP BY day
+ORDER BY total DESC ;
 /*
  3. (по желанию) Подсчитайте произведение чисел в столбце таблицы.
  */
+-- совпало с разбором
 USE storehouse;
 SELECT value
 FROM storehouses_products;
-SELECT EXP(SUM(LOG(value))) AS multiply
+SELECT ROUND(EXP(SUM(LN(value)))) AS multiply
 FROM storehouses_products; -- логарифм произведения равен сумме логарифмов
