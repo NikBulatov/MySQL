c19df522a5e5fc05343b10fc0abf2092bbf0a7c5
Nikita <anthrowolf@ya.ru>
2021-12-03 10:26:00 +0300

    Как правильно

--- Scripts/task.sql
+++ Scripts/task.sql
@@ -5,6 +5,7 @@
  */
 USE vk;
 -- Саша помог
+/*
 SELECT from_user_id AS target_result, to_user_id AS our_user, amount
 FROM (
          SELECT messages.from_user_id, messages.to_user_id, COUNT(DISTINCT messages.id) AS amount
@@ -19,17 +20,36 @@ FROM (
                        ON initiator_user_id IN (mess.from_user_id, mess.to_user_id) AND status = 'approved'
          WHERE mess.from_user_id = 1
          GROUP BY from_user_id, to_user_id) AS result
+GROUP BY from_user_id
 ORDER BY amount DESC
 LIMIT 1;
-
--- 2. Подсчитать общее количество лайков, которые получили пользователи младше 10 лет.
-SELECT COUNT(*)
+  */
+-- решение препода
+SELECT from_user_id AS friend,
+       (SELECT CONCAT(firstname, ' ', lastname) FROM users WHERE id = messages.from_user_id) AS name,
+       COUNT(*) AS messages
+FROM messages
+WHERE to_user_id = 1
+  AND from_user_id IN (SELECT initiator_user_id
+                       FROM friend_requests
+                       WHERE (target_user_id = 1)
+                         AND status = 'approved'
+                       UNION
+                       SELECT target_user_id
+                       FROM friend_requests
+                       WHERE (initiator_user_id = 1)
+                         AND status = 'approved')
+GROUP BY friend
+ORDER BY messages DESC
+LIMIT 1;
+-- 2. Подсчитать общее количество лайков, которые ПОЛУЧИЛИ пользователи младше 10 лет.
+SELECT COUNT(id) AS amount
 FROM likes
-         JOIN
-     profiles
-     ON likes.user_id = profiles.user_id
-         AND TIMESTAMPDIFF(YEAR, birthday, NOW()) < 10;
-
+WHERE media_id IN (SELECT id
+                   FROM media
+                   WHERE user_id IN (SELECT user_id
+                                     FROM profiles
+                                     WHERE TIMESTAMPDIFF(YEAR, birthday, NOW()) < 10));
 -- 3. Определить кто больше поставил лайков (всего): мужчины или женщины.
 SELECT COUNT(*) AS amount, gender
 FROM likes
