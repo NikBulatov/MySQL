271e5adc68591c45c8290c59a5241275066eec99
Nikita <anthrowolf@ya.ru>
2021-12-07 14:11:18 +0300

    Практическое задание по теме "Хранимые процедуры и функции, триггеры"

--- Scripts/task.sql
+++ Scripts/task.sql
@@ -20,7 +20,11 @@ FROM shop.users;
 /* 2. Создайте представление, которое выводит название name товарной позиции из таблицы products
    и соответствующее название каталога name из таблицы catalogs.
  */
-USE samples;
+
+DROP VIEW IF EXISTS v_products_and_catalogs;
+CREATE OR REPLACE VIEW v_products_and_catalogs AS
+SELECT id, name, (SELECT name FROM catalogs WHERE catalog_id = catalogs.id) AS catalog_name
+FROM products;
 SELECT id, name, catalog_name
 FROM v_products_and_catalogs;
 
