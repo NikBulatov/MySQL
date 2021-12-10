12b5e225af5b12e4e76c61497a4249df0c02d6aa
Nikita <anthrowolf@ya.ru>
2021-12-10 13:15:35 +0300

    Черновик

--- Scripts/task.sql
+++ Scripts/task.sql
@@ -8,5 +8,21 @@
 2) Создайте SQL-запрос, который помещает в таблицу users миллион записей.
  */
 -- 1
+USE samples;
+DROP TABLE IF EXISTS `logs`;
+CREATE TABLE `logs`
+(
+    id SERIAL PRIMARY KEY,
+    user_id BIGINT UNSIGNED NOT NULL,
+    catalog_id BIGINT UNSIGNED NOT NULL,
+    product_id BIGINT UNSIGNED NOT NULL,
+    table_name VARCHAR(255),
+    catalog_updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    product_updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    user_updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 
+    FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (catalog_id) REFERENCES catalogs(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES products(id) ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE = Archive;
 -- 2
