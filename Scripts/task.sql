efebc7efe6deeb0875d7f7c05940e3b37c2e97c7
Nikita <anthrowolf@ya.ru>
2021-12-11 10:38:11 +0300

    Практическое задание по теме “Оптимизация запросов”

--- Scripts/task.sql
+++ Scripts/task.sql
@@ -8,21 +8,67 @@
 2) Создайте SQL-запрос, который помещает в таблицу users миллион записей.
  */
 -- 1
-USE samples;
+USE shop;
 DROP TABLE IF EXISTS `logs`;
 CREATE TABLE `logs`
 (
-    id SERIAL PRIMARY KEY,
-    user_id BIGINT UNSIGNED NOT NULL,
-    catalog_id BIGINT UNSIGNED NOT NULL,
-    product_id BIGINT UNSIGNED NOT NULL,
-    table_name VARCHAR(255),
-    catalog_updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    product_updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    user_updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-
-    FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (catalog_id) REFERENCES catalogs(id) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (product_id) REFERENCES products(id) ON UPDATE CASCADE ON DELETE CASCADE
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    table_name VARCHAR(255)                       NOT NULL,
+    id_pk      BIGINT UNSIGNED                    NOT NULL,
+    row_name   VARCHAR(255)                       NOT NULL
 ) ENGINE = Archive;
+
+DROP TRIGGER IF EXISTS users_log;
+DELIMITER //
+CREATE TRIGGER users_log
+    AFTER INSERT
+    ON users
+    FOR EACH ROW
+BEGIN
+    INSERT INTO logs
+    SET table_name = 'users',
+        id_pk      = NEW.id,
+        row_name   = NEW.name;
+END //
+
+DROP TRIGGER IF EXISTS catalogs_log//
+CREATE TRIGGER catalogs_log
+    AFTER INSERT
+    ON catalogs
+    FOR EACH ROW
+BEGIN
+    INSERT INTO logs
+    SET table_name = 'catalogs',
+        id_pk      = NEW.id,
+        row_name   = NEW.name;
+END //
+DROP TRIGGER IF EXISTS products_log//
+CREATE TRIGGER products_log
+    AFTER INSERT
+    ON products
+    FOR EACH ROW
+BEGIN
+    INSERT INTO logs
+    SET table_name = 'products',
+        id_pk      = NEW.id,
+        row_name   = NEW.name;
+END //
+DELIMITER ;
 -- 2
+DROP PROCEDURE IF EXISTS insert_million;
+DELIMITER //
+CREATE PROCEDURE insert_million()
+BEGIN
+    DECLARE count INT DEFAULT 7;
+    WHILE count < 1000000
+        DO
+            INSERT INTO users (name, birthday_at)
+            VALUES (CONCAT('user_', count), NOW());
+            SET count = count + 1;
+        END WHILE;
+END//
+DELIMITER ;
+-- checking
+CALL insert_million();
+SELECT id
+FROM users;
